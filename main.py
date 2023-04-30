dc7b2ec5623480041caa8f90b34700c1e21d0b69
Salah Yasser <Salahy.allaithy@gmail.com>
2023-05-01 00:39:15 +0300

    addtion specialty for doctor 1.8

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def receive_message():
                                                      "Please describe what do you feel in detail to help you ^_^ ")
                                     elif "doctor" in firstText or "Doctor" in firstText or "DOCTOR" in firstText or "DR" in firstText or "Dr" in firstText or "dr" in firstText:
                                         cur4 = conn.execute("INSERT INTO doctor (id, specialty ) VALUES (?,?)",
-                                                            (recipient_id, "None"))
+                                                            (recipient_id, specialty))
                                         conn.commit()
                                         send_message(recipient_id, "welcome Dr " + " <3 ")
                                         send_message(recipient_id,
