b4702c5298f79bfc703e7df37c456f9408aa3295
Salah Yasser <Salahy.allaithy@gmail.com>
2023-05-07 15:49:55 +0300

    g0maa 1.3

--- main.py
+++ main.py
@@ -155,11 +155,11 @@ def all_conversation(response_sent_text, firstText, patientList, doctorsList, re
             elif "good bye" in firstText or "Good Bye" in firstText or "Good bye" in firstText or "bye" in firstText or "Bye" in firstText:
                 send_message(recipient_id, firstText + " Dr " + " ^_^ ")
             elif "oncology" in firstText:
-                cur9 = conn.execute(
-                    "UPDATE doctor SET speciality = " + firstText +  " WHERE id = " + recipient_id
-                    )
-                conn.commit()
-                conn.close()
+                # cur9 = conn.execute(
+                #     "UPDATE doctor SET speciality = " + firstText +  " WHERE id = " + recipient_id
+                #     )
+                # conn.commit()
+                # conn.close()
                 send_message(recipient_id, "sepciality" + firstText)
             # elif "dev" in firstText:
             #     cur10 = conn.execute(
